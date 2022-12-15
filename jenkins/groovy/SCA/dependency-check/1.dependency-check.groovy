b49ae075f10ba7ddec46729098b86de2f973a538
OneDayGinger <hsremnant9@gmail.com>
2022-12-16 01:58:35 +0900

    Update 1.dependency-check.groovy

--- jenkins/groovy/SCA/dependency-check/1.dependency-check.groovy
+++ jenkins/groovy/SCA/dependency-check/1.dependency-check.groovy
@@ -1,3 +1,5 @@
 stage('dependency-check') {
-  sh components_dir + 'SCA/dependency-check/script/run.sh' + " ${env.WORKSPACE}"
+  def depencency_check_dir = 'SCA/dependency-check/script/run.sh'
+  sh 'chmod +x ' + components_dir + dependency_check_dir
+  sh components_dir + dependency_check_dir + " ${env.WORKSPACE}"
 }
