c18c0687b866ae5b281459962d9f863cda29f04c
OneDayGinger <hsremnant9@gmail.com>
2022-12-16 02:01:39 +0900

    Update 1.gitleaks.groovy

--- jenkins/groovy/SIS/gitleaks/1.gitleaks.groovy
+++ jenkins/groovy/SIS/gitleaks/1.gitleaks.groovy
@@ -1,3 +1,5 @@
-stage('ggshield') {
-  sh components_dir + 'SIS/gitleaks/script/run.sh' + " ${env.WORKSPACE}"
+stage('gitleaks') {
+  def gitleaks_dir = 'SIS/gitleaks/script/run.sh'
+  sh 'chmod +x ' + components_dir + gitleaks_dir
+  sh components_dir + gitleaks_dir + " ${env.WORKSPACE}"
 }
