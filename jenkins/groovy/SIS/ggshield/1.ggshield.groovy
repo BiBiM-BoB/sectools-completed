a7a05ddd72fd2e2eb17fcc36f811f5ac11eee66a
OneDayGinger <hsremnant9@gmail.com>
2022-12-17 18:15:59 +0900

    Update 1.ggshield.groovy

--- jenkins/groovy/SIS/ggshield/1.ggshield.groovy
+++ jenkins/groovy/SIS/ggshield/1.ggshield.groovy
@@ -1,5 +1,5 @@
 stage('ggshield') {
   def ggshield_dir = 'SIS/ggshield/script/run.sh'
   sh 'chmod +x ' + components_dir + ggshield_dir
-  sh components_dir + 'SIS/ggshield/script/run.sh' + " ${env.WORKSPACE}"
+  sh components_dir + 'SIS/ggshield/script/run.sh' + " ${env.WORKSPACE}" + " ${pipeline_name}"
 }
