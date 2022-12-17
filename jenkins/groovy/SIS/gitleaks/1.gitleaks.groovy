f1320db2140f8442b55c14d9cc894e53ede43610
OneDayGinger <hsremnant9@gmail.com>
2022-12-17 19:57:09 +0900

    Update 1.gitleaks.groovy

--- jenkins/groovy/SIS/gitleaks/1.gitleaks.groovy
+++ jenkins/groovy/SIS/gitleaks/1.gitleaks.groovy
@@ -1,5 +1,5 @@
 stage('gitleaks') {
   def gitleaks_dir = 'SIS/gitleaks/script/run.sh'
   sh 'chmod +x ' + components_dir + gitleaks_dir
-  sh components_dir + gitleaks_dir + " ${env.WORKSPACE}"
+  sh components_dir + gitleaks_dir + " ${env.WORKSPACE}" + " ${pipeline_name}"
 }
