51c7a89c11f9230bb786ac74f4d2ce22fd90ec56
OneDayGinger <hsremnant9@gmail.com>
2022-12-17 17:49:27 +0900

    Update run.sh

--- components/SCA/dependency-check/script/run.sh
+++ components/SCA/dependency-check/script/run.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
-SYNTAX="syntax: $0 <target_dir>"
+SYNTAX="syntax: $0 <target_dir> <pipeline_name>"
 
 if [ $# -eq "0" ]; then
-    echo "Target directory is required"
+    echo "Target directory and Pipeline name are required"
     echo "$SYNTAX"
     exit 1
 fi
 
+# Tool Meta Data
+STAGE="SCA"
+TOOL="dependency-check"
+REPORT_MANAGER_DIRECTORY="/var/jenkins_home/userContent/components/Report_manager.py"
+
 VERSION="latest"
 BIBIMBOB_DIRECTORY=$HOME/bibim
 TARGET_DIRECTORY="$1"
+PIPELINE_NAME="$2"
 REPORT_DIRECTORY="$BIBIMBOB_DIRECTORY/report"
 REPORT_FORMAT="json"
-REPORT_FILE_NAME="sis-ggshield-report.$REPORT_FORMAT"
+REPORT_FILE_NAME="sca-dependency-check-report.$REPORT_FORMAT"
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
@@ -26,9 +32,9 @@ echo "ggshield: target directory is $TARGET_DIRECTORY"
 ls -al $TARGET_DIRECTORY
 echo "ggshield: report directory is $REPORT_DIRECTORY"
 
-cp $SCRIPTPATH/Dockerfile ./ggshield_Dockerfile
+cp $SCRIPTPATH/Dockerfile ./dependency-check_Dockerfile
 
-docker build -t ggshield:$VERSION -f ggshield_Dockerfile .
+docker build -t dependency-check:$VERSION -f dependency-check_Dockerfile .
 
 docker run --rm \
     --env GITGUARDIAN_API_KEY=1 \
