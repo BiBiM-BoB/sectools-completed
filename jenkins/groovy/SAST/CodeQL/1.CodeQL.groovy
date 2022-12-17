9726ad882b96cd8604e1b7b5ac168ef1a401fd3b
OneDayGinger <hsremnant9@gmail.com>
2022-12-18 03:35:44 +0900

    Pushed components of SAST / CodeQL

---
+++ jenkins/groovy/SAST/CodeQL/1.CodeQL.groovy
@@ -0,0 +1,6 @@
+stage('CodeQL') {
+  sh "ls -al"
+  def gitleaks_dir = 'SIS/gitleaks/script/run.sh'
+  sh 'chmod +x ' + components_dir + gitleaks_dir
+  sh components_dir + gitleaks_dir + " ${env.WORKSPACE}" + " ${pipeline_name}"
+}
