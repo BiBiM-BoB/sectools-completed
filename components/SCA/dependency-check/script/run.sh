953f7e8ea74d21a2e5c5b1aa17cf8875c94e344f
OneDayGinger <hsremnant9@gmail.com>
2022-12-17 17:57:54 +0900

    Update run.sh

--- components/SCA/dependency-check/script/run.sh
+++ components/SCA/dependency-check/script/run.sh
@@ -13,35 +13,48 @@ STAGE="SCA"
 TOOL="dependency-check"
 REPORT_MANAGER_DIRECTORY="/var/jenkins_home/userContent/components/Report_manager.py"
 
-VERSION="latest"
 BIBIMBOB_DIRECTORY=$HOME/bibim
 TARGET_DIRECTORY="$1"
 PIPELINE_NAME="$2"
 REPORT_DIRECTORY="$BIBIMBOB_DIRECTORY/report"
-REPORT_FORMAT="json"
-REPORT_FILE_NAME="sca-dependency-check-report.$REPORT_FORMAT"
+REPORT_FORMAT="JSON"
+
+DC_VERSION="latest"
+DC_DIRECTORY="$BIBIMBOB_DIRECTORY/OWASP-Dependency-Check"
+DC_PROJECT="dependency-check scan: $(pwd)"
+DATA_DIRECTORY="$DC_DIRECTORY/data"
+CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
+
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-if [ ! -d "$REPORT_DIRECTORY" ]; then
-    echo "Initially creating persistent directory: $REPORT_DIRECTORY"
-    mkdir -p "$REPORT_DIRECTORY"
+if [ ! -d "$DATA_DIRECTORY" ]; then
+    echo "Initially creating persistent directory: $DATA_DIRECTORY"
+    mkdir -p "$DATA_DIRECTORY"
+fi
+if [ ! -d "$CACHE_DIRECTORY" ]; then
+    echo "Initially creating persistent directory: $CACHE_DIRECTORY"
+    mkdir -p "$CACHE_DIRECTORY"
 fi
 
-echo "ggshield: target directory is $TARGET_DIRECTORY"
+echo "dependency-check: target directory is $TARGET_DIRECTORY"
 ls -al $TARGET_DIRECTORY
-echo "ggshield: report directory is $REPORT_DIRECTORY"
+echo "dependency-check: report directory is $REPORT_DIRECTORY"
 
 cp $SCRIPTPATH/Dockerfile ./dependency-check_Dockerfile
 
-docker build -t dependency-check:$VERSION -f dependency-check_Dockerfile .
+docker build -t dependency-check:$DC_VERSION -f dependency-check_Dockerfile .
 
 docker run --rm \
-    --env GITGUARDIAN_API_KEY=1 \
-    --volume "$REPORT_DIRECTORY":/report \
-    ggshield:$VERSION secret scan \
-    --json \
-    path . --recursive -y > $REPORT_DIRECTORY/$REPORT_FILE_NAME || true
+    -e user=$USER \
+    -u $(id -u ${USER}):$(id -g ${USER}) \
+    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data:z \
+    --volume "$REPORT_DIRECTORY":/report:z \
+    dependency-check:$DC_VERSION \
+    --scan /src \
+    --format "$REPORT_FORMAT" \
+    --project "$DC_PROJECT" \
+    --out /report=
 
 echo "ggshield: scanning completed."
 
